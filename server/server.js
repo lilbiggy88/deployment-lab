194391d302002c56a71d3d7a400f0c36087d23d2
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-11-10 23:39:05 -0700

    added index.html in derver

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 4005;
 
