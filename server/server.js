941e3067e06aea6d047ea702c464e78a24cb5e1a
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-11-10 23:54:26 -0700

    brought it back to the OG

--- server/server.js
+++ server/server.js
@@ -6,25 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Middleware
-app.use(express.static(path.join(__dirname, "../public")));
-// app.use("/styles", express.static(path.join(__dirname, "../public/index.css")));
-//Endpoints
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public"));
-// })
-
-// app.get("/", function(req,res) {
-//     // res.sendFile(path.join(__dirname, "../index.html")) before getting it to heroku
-//     res.sendFile(path.join(__dirname, "../public/index.html"))
-// });
-
-// app.get("/styles", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/index.css"));
-// });
+app.use(express.static(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
-    console.log(`We vibin on port ${port}`)
+    console.log(`We chillin on port ${port}`)
 });
