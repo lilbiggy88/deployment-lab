fa86e3a791d08246e9fbdb0e99097c31c1b7ce1c
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-11-10 23:49:56 -0700

    trying on the server

--- server/server.js
+++ server/server.js
@@ -6,10 +6,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../public/index.html")));
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")));
+// app.use("/styles", express.static(path.join(__dirname, "../public/index.css")));
+//Endpoints
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public"));
+// })
+
+// app.get("/", function(req,res) {
+//     // res.sendFile(path.join(__dirname, "../index.html")) before getting it to heroku
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// });
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
-    console.log(`We chillin on port ${port}`)
+    console.log(`We vibin on port ${port}`)
 });
